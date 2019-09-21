c3c76c7c6b74eebfa858b7d3ce6e6ec29c2f7a3e
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-09-21 18:52:31 -0300

    Add files via upload

---
+++ Modulo01.R
@@ -0,0 +1,86 @@
+# Sejam Vindos ao minicurso de introducao ao R!
+
+# Modulo 1: Operacoes Basicas de Matematicas
+
+# Soma
+
+5 + 6
+
+7 + 34765
+
+43527 + 98690 + 3029 + 2
+
+# Subtracao
+
+348 - 98
+
+3648 - 4500
+
+3726 - 936 - 5894
+
+# Multiplicacao
+
+456 * 34
+
+2 * 350
+
+3 * 5 + 1
+
+(5 + 1) * 3
+
+# Divisao
+
+50 / 2
+
+4637 / 4
+
+24 / 2 + 5
+
+(2 + 5) / 24
+
+# Potencia
+
+3^2
+
+34^7
+
+2^2 - 3
+
+(2 - 3) ^ 2 
+
+# Raiz
+
+sqrt(9)
+
+sqrt(36) * 5
+
+sqrt(25) + 6
+
+# Outras operacoes 
+
+exp(0) # Exponencial
+
+log(1) # Log na base e
+
+log10(1) # Log na base 10
+
+########################## Exercícos #######################
+
+# 1. Calcule:
+
+# Letra a - A soma do dia que você nasceu mais o mês e o ano. 
+# Depois multiplique por 3.
+
+(28 + 03 + 1997) * 3
+
+# Letra b - Faça 5 elevado a 5, depois pegue o resultado e divida por 100
+
+(5 ^ 5) / 100
+
+# Letra c - Faça o exponencial de 7, divida por 2 e subtraia 5
+
+exp(7) / 2 - 5
+
+
+
+
